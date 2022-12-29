6138cf9c9983e10cb007d519ef90c2a117eec45c
Timm Knape <timm@knp.de>
2022-12-29 22:03:08 +0100

    move to implementation section

--- retriever.cpp
+++ retriever.cpp
@@ -1,5 +1,22 @@
-//
-// Created by Timm Knape on 29.12.22.
-//
-
 #include "retriever.h"
+
+[[nodiscard]] Retriever::Value_Type Retriever::get(
+    const Retriever::Path &path, unsigned version
+) const {
+    auto v_path { base / path / std::to_string(version) };
+    auto in {std::make_unique<std::ifstream>(v_path)};
+    if (in->fail()) { return std::nullopt; }
+    return Value_Type {std::move(in)};
+}
+
+[[nodiscard]] Retriever::Value_Type Retriever::get(
+    const Retriever::Path &path, const char *tag
+) const {
+    if (! tag || ! *tag || isdigit(*tag)) { return std::nullopt; }
+    auto tag_path { base / path / tag };
+    std::ifstream tag_in(tag_path);
+    unsigned v;
+    tag_in >> v;
+    if (tag_in.fail()) { return std::nullopt; }
+    return get(path, v);
+}
--- retriever.h
+++ retriever.h
@@ -16,22 +16,9 @@ class Retriever {
 
         Path base {std::filesystem::current_path()};
 
-        [[nodiscard]] Value_Type get(const Path &path, unsigned version) const {
-            auto v_path { base / path / std::to_string(version) };
-            auto in {std::make_unique<std::ifstream>(v_path)};
-            if (in->fail()) { return std::nullopt; }
-            return Value_Type {std::move(in)};
-        }
+        [[nodiscard]] Value_Type get(const Path &path, unsigned version) const;
 
-        [[nodiscard]] Value_Type get(const Path &path, const char *tag) const {
-            if (! tag || ! *tag || isdigit(*tag)) { return std::nullopt; }
-            auto tag_path { base / path / tag };
-            std::ifstream tag_in(tag_path);
-            unsigned v;
-            tag_in >> v;
-            if (tag_in.fail()) { return std::nullopt; }
-            return get(path, v);
-        }
+        [[nodiscard]] Value_Type get(const Path &path, const char *tag) const;
 
         [[nodiscard]] Value_Type get(const Path &path) const { return get(path, LATEST_TAG); }
 };
